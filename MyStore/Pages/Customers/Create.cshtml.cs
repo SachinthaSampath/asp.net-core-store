03e731c20436e42d952978615df8605a6addfd80
Sachintha Sampath <sachinsampath42@gmail.com>
2023-08-26 01:02:07 +0530

    chore: Initial Commit

---
+++ MyStore/Pages/Customers/Create.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Customers
+{
+    public class CreateModel : PageModel
+    {
+		public CustomerInfo customerInfo = new CustomerInfo();
+		public string errorMessage = "";
+		public string successMessage = "";
+
+        public void OnGet()
+        {
+
+        }
+		public void OnPost()
+		{
+			customerInfo.address = Request.Form["address"];
+			customerInfo.contaxt = Request.Form["contaxt"];
+			customerInfo.email = Request.Form["email"];
+			customerInfo.name = Request.Form["name"];
+
+			if (customerInfo.name.Length == 0)
+			{
+				errorMessage = "All the fields are requred";
+				return;
+			}
+
+			//save the client to the database
+
+			try
+			{
+				String conString = "Data Source=DESKTOP-6EV1VUB;Initial Catalog=ecom-app;Integrated Security=True";
+				using (SqlConnection con = new SqlConnection(conString))
+				{
+					con.Open();
+					String sql = "INSERT INTO clients (name,email,address,contaxt) VALUES (@name,@email,@address,@contaxt);";
+					using (SqlCommand cmd = new SqlCommand(sql, con))
+					{
+						cmd.Parameters.AddWithValue("@name", customerInfo.name);
+						cmd.Parameters.AddWithValue("@email", customerInfo.email);
+						cmd.Parameters.AddWithValue("@address", customerInfo.address);
+						cmd.Parameters.AddWithValue("@contaxt", customerInfo.contaxt);
+
+						cmd.ExecuteNonQuery();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			customerInfo.name = "";
+			customerInfo.address = "";
+			customerInfo.contaxt = "";
+			customerInfo.email = "";
+
+			Response.Redirect("/Customers");
+		}
+    }
+
+	public class CustomerInfo
+	{
+		public string id;
+		public string name;
+		public string email;
+		public string address;
+		public string contaxt;
+	}
+
+}
---
+++ MyStore/Pages/Customers/index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Customers
+{
+	
+
+	public class indexModel : PageModel
+	{
+		public List<CustomerInfo> customerList = new List<CustomerInfo>();
+
+		public void OnGet()
+		{
+			try
+			{
+				String conString = "Data Source=DESKTOP-6EV1VUB;Initial Catalog=ecom-app;Integrated Security=True";
+				using (SqlConnection con = new SqlConnection(conString))
+				{
+					con.Open();
+					String sql = "SELECT * FROM customer";
+					using (SqlCommand cmd = new SqlCommand(sql, con))
+					{
+						using (SqlDataReader reader = cmd.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								CustomerInfo ci = new CustomerInfo();
+								ci.id = "" + reader.GetInt32(0);
+								ci.name = reader.GetString(1);
+								ci.email = reader.GetString(2);
+								ci.address = reader.GetString(3);
+								ci.contaxt = reader.GetString(4);
+								customerList.Add(ci);
+							}
+						}
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
+
+
+class CustomerInfo
+{
+	public string id;
+	public string name;
+	public string email;
+	public string address;
+	public string contaxt;
+}
